8bfff5b55b474a967dd758164f2b9577be984e05
silverywaves <babyshu1210@gmail.com>
2024-07-15 19:13:32 +0900

    react bootstrap 설치 및 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'; // 여기서도 import 해줘야 사용가능
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar bg="dark" data-bs-theme="dark">
+				<Container>
+					<Navbar.Brand href="#home">Shop</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+						<Nav.Link href="#pricing">Pricing</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+			<Button variant="info">Info</Button>
+		</div>
+	);
 }
 
 export default App;
